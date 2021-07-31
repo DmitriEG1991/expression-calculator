d356207ac7f6fd64f0242322d499550159754b59
DmitriEG1991 <78304061+DmitriEG1991@users.noreply.github.com>
2021-08-01 01:02:28 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function eval() {
-    // Do not use eval!!!
-    return;
+function expressionCalculator(w) {
+    let a = w.match(/[(]/g);
+    let b = w.match(/[)]/g);
+    let c = w.match(/\/ 0/g);
+    if(c){
+        throw 'TypeError: Division by zero.';
+    } 
+    if(a && b && a.length !== b.length){
+        throw 'ExpressionError: Brackets must be paired';
+    }
+    if(!a && b){
+        throw 'ExpressionError: Brackets must be paired';
+    }
+    if(a && !b){
+        throw 'ExpressionError: Brackets must be paired';
+    }
+    const res = new Function(`return ${w}`);
+    let dom = res();
+    if(dom == Infinity){
+        throw "TypeError: Division by zero.";
+    }  
+    return result;
 }
-
-function expressionCalculator(expr) {
-    // write your solution here
-}
-
 module.exports = {
     expressionCalculator
 }
